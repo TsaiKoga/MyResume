fb8bc5e898168a03ec39daccc53cb4d154ddb8a2
TsaiKoga <969024089@qq.com>
2018-10-17 17:27:34 +0800

    [Add]Add jquery and output text

--- js/index.js
+++ js/index.js
@@ -1,18 +1,64 @@
-var str = document.getElementById('resume').innerHTML.toString();
-var i = 0;
-document.getElementById('resume').innerHTML = "";
+/**
+ * @author: TsaiKoga
+ * @create_date: 2018-10-15
+ */
+
+var $commands = $('#resume').find('.command');
+var commandsStr = [];
+
+$commands.each(function(index) {
+    var len = this.innerHTML.length;
+    commandsStr.push(this.innerHTML);
+    this.innerHTML = "";
+});
 
 
-setTimeout(function() {
+function runCommand(index) {
+    var i = 0;
+    var cmdT = setTimeout(function() {
+        typeCharToDisplayResult(i, index);
+    },0);
+    if (i > commandsStr[index].length) {
+        clearTimeout(cmdT);
+    }
+}
+
+function typeCharToDisplayResult(i, index) {
     var se = setInterval(function() {
         i++;
-        document.getElementById('resume').innerHTML = str.slice(0, i) + "|";
+        $commands[index].innerHTML = commandsStr[index].slice(0, i) + "|";
         var resume = document.getElementById("resume");
         var screen = document.getElementById("screen");
         screen.scrollTop = screen.scrollHeight - screen.clientHeight;
-        if (i == str.length) {
+        console.log(screen.scrollTop);
+
+        if (i == commandsStr[index].length) {
             clearInterval(se);
-            document.getElementById('resume').innerHTML = str;
+            $commands[index].innerHTML = commandsStr[index];
+            $($commands[index]).next('.direct-output').css('display', 'block');
         }
     }, 10);
-},0);
+}
+
+// var firstT = setTimeout(function() {
+//     runCommand(0);
+//     var secT = setTimeout(function() {
+//         runCommand(1);
+//         var thirdT = setTimeout(function() {
+//             runCommand(2);
+//             console.log(3);
+//         }, 1000);
+//         console.log(2);
+//     }, 1000);
+//     console.log(1);
+// }, 1000);
+function serialRunCMD(index) {
+    setTimeout(function() {
+        runCommand(index);
+        if (index < $commands.length - 1) {
+            serialRunCMD(index+1);
+        }
+    }, 1000);
+}
+
+serialRunCMD(0);
